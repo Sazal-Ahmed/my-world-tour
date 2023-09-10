be149e30606addb3993ac73b82f073c8b5427599
sazal <sazalsiu@gmail.com>
2023-09-10 19:31:39 +0600

    visited button added and conditional css added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 
 import ('./Country.css');
 const Country = ({country}) => {
     console.log(country);
 
-        const {name, flags, population , area
+        const {name, flags, population , area, cca3
         } = country;
+
+        const [visible, setVisible] = useState(false);
+
+        const handleVisible = () => {
+            setVisible(!visible);
+        }
    
     return (
-        <div className='country'>
+        <div className={`country ${visible && 'visited'}`}>
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
+            <p><small>Code :{cca3}</small></p>
+            <button onClick={handleVisible}>{visible?'visited' : 'going'}</button>
+            {
+                visible ? 'I am visited this country' : 'I want to visit'
+            }
         </div>
     );
 };
